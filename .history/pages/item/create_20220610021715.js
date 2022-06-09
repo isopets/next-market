d7b9cbfe188f5824a28b31aabf6b938a29833389
yuto isogai <jandp.0717@gmail.com>
2022-06-10 02:19:19 +0900

    create.js form input & textarea add state

---
+++ .history/pages/item/create_20220610021715.js
@@ -0,0 +1,27 @@
+import {useState} from "react";
+const CreateItem = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div>
+      <h1>アイテム作成</h1>
+      <form>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" name="title" placeholder="アイテム名" required />
+        <input value={title} onChange={(e) => setTitle(e.target.value)}  type="text" name="price" placeholder="価格" required />
+        <input value={title} onChange={(e) => setTitle(e.target.value)}  type="text" name="image" placeholder="画像" required />
+        <textarea
+          type="text"
+          name="description"
+          rows="15"
+          placeholder="商品説明"
+          required></textarea>
+        <button>作成</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateItem;
